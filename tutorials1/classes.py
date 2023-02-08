11e1cc4d3f899697ccacba9fbde3d3689d00aa94
Jakey <111139747+jctacogue@users.noreply.github.com>
2023-02-08 18:21:27 +0800

    try

---
+++ tutorials1/classes.py
@@ -0,0 +1,26 @@
+class Pets:
+    looks = "Gwapo!"
+
+print(Pets().looks)
+
+#You can say that HTML classes and Python classes have the same use cases, with some subtle differences.
+#
+# HTML/CSS lets you format classes like this:
+#
+# [CSS]
+# .class {
+#   background-color: blue;
+# }
+#
+# whereas, Python takes few longer lines of code, and is encouraged to assign an object to your newly created Class:
+#
+# class Class():
+#   background-color = "Blue"
+#
+# object = Class()
+#
+# [CONSOLE]
+# print(object.background-color)
+# Blue
+#
+
